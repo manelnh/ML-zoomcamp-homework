4daf8bd6825327d94f0ea7b916e323140d7d1656
manel nahdi <manel.nahdi732@gmail.com>
2025-11-04 17:10:24 +0100

    Add files via upload

---
+++ 05- Deployment/pickle_score.py
@@ -0,0 +1,15 @@
+import pickle
+
+
+with open("pipeline_v1.bin", "rb") as f_in:
+    pipeline = pickle.load(f_in)
+
+
+client={
+    "lead_source": "paid_ads",
+    "number_of_courses_viewed": 2,
+    "annual_income": 79276.0
+}
+
+probability = pipeline.predict_proba([client])[0, 1]
+print("Conversion probablity:", probability)
